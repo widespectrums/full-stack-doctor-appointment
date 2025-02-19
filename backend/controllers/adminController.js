8d5ec6cdafffda960e75b14bc850ec5cb6f8337e
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-02-19 19:54:00 +0300

    method added

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -67,5 +67,15 @@ const loginAdmin = async (req, res) => {
         res.json({ success: false, message: error.message })
     }
 }
+//API to get all doctors list for admin panel
+const allDoctors = async (req, res) => {
+    try{
+        const doctors = await doctorModel.find({}).select("-password")
+        res.json({success: true, doctors})
 
-export{addDoctor, loginAdmin}
+    }catch(error){
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+export{addDoctor, loginAdmin, allDoctors}
