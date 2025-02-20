a2284771fa289d97f196c491c1862ea00cac8af9
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-02-20 22:26:00 +0300

    doctorList function added

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -11,4 +11,13 @@ const changeAvailability = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
-export {changeAvailability}
+const doctorList = async (req, res) => {
+    try{
+        const doctors = await doctorModel.find({}).select(["-password" , "-email"])
+        res.json({success: true, doctors})
+    }catch(error){
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+export {changeAvailability,doctorList}
