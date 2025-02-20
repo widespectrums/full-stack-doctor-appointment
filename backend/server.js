40468d534bd37e5a42b799d695d8996767652ec5
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-02-20 22:27:28 +0300

    router updated

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import "dotenv/config"
 import connectCloudinary from "./config/cloudinary.js";
 import connectDB from "./config/mongodb.js";
 import adminRouter from "./routes/adminRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
 
 //app config
 const app = express()
@@ -17,7 +18,7 @@ app.use(cors());
 
 // api endpoints
 app.use('/api/admin', adminRouter)
-
+app.use("/api/doctor" , doctorRouter)
 app.get('/', (req, res) => {
     res.send("API WORKING")
 })
