82a288501f508c906024b52e5d809fe9bd32346a
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-03-01 01:38:14 +0300

    userRoutes updated

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,16 +1,17 @@
 import express from "express";
-import {addDoctor, loginAdmin} from "../controllers/adminController.js";
+import {addDoctor, allDoctors, loginAdmin} from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
-import authAdmin from "../middlewares/auth.js";
+import authAdmin from "../middlewares/authAdmin.js";
+import {changeAvailability} from "../controllers/doctorController.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/login", loginAdmin)
 adminRouter.post("/add-doctor", authAdmin, upload.single('image'), addDoctor)
-adminRouter.get("/appointments", authAdmin, appointmentsAdmin)
-adminRouter.post("/cancel-appointment", authAdmin, appointmentCancel)
+// adminRouter.get("/appointments", authAdmin, appointmentsAdmin)
+// adminRouter.post("/cancel-appointment", authAdmin, appointmentCancel)
 adminRouter.get("/all-doctors", authAdmin, allDoctors)
-adminRouter.post("/change-availability", authAdmin, changeAvailablity)
-adminRouter.get("/dashboard", authAdmin, adminDashboard)
+adminRouter.post("/change-availability", authAdmin, changeAvailability)
+// adminRouter.get("/dashboard", authAdmin, adminDashboard)
 
 export default adminRouter;
