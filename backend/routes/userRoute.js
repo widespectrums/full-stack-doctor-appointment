43edfc80649a0813a3296e166d2608672d149ce7
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-03-01 01:38:29 +0300

    userRoutes updated

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,7 +1,15 @@
 import express from "express";
-import {loginUser, registerUser} from "../controllers/userController.js";
+import {getProfile, loginUser, registerUser, updateProfile, bookAppointment, listAppointment} from "../controllers/userController.js";
+import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
-userRouter.post("/register" , registerUser);
-userRouter.post("/login" , loginUser);
+userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+
+userRouter.get("/get-profile", authUser, getProfile)
+userRouter.post("/update-profile", authUser, upload.single("image"), updateProfile);
+userRouter.post("/book-appointment", authUser, bookAppointment);
+userRouter.get("/appointments", authUser, listAppointment);
+
 export default userRouter;
