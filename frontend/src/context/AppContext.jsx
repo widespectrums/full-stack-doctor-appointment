99a05e14cc3cd2355d81306710e307b54c558591
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-02-28 16:43:09 +0300

    AppContext.js updated

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,30 +6,53 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
     const currencySymbol = "$"
-    const backendUrl= import.meta.env.VITE_BACKEND_URL;
-    const[doctors, setDoctors] = useState([]);
-    const[token, setToken] = useState(localStorage.getItem("token")? localStorage.getItem("token"): false);
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [doctors, setDoctors] = useState([]);
+    const [token, setToken] = useState(localStorage.getItem("token") ? localStorage.getItem("token") : false);
+    const [userData, setUserData] = useState(false);
 
     const getDoctorsData = async () => {
-        try{
-            const { data } = await axios.get(backendUrl + "/api/doctor/list")
-            if(data.success){
+        try {
+            const {data} = await axios.get(backendUrl + "/api/doctor/list")
+            if (data.success) {
                 setDoctors(data.doctors)
-            }else{
+            } else {
                 toast.error(data.message)
             }
-        }catch (error){
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+    const loadUserProfileData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + "/api/users/get-profile", {headers: {token}});
+            if (data.success) {
+                setUserData(data.userData);
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
             console.log(error)
             toast.error(error.message)
         }
     }
     const value = {
         doctors,
-        currencySymbol,token, setToken,backendUrl
+        currencySymbol, token, setToken, backendUrl, userData, setUserData, loadUserProfileData,
     }
     useEffect(() => {
         getDoctorsData()
     }, [])
+
+    useEffect(() => {
+        if (token) {
+            loadUserProfileData()
+        } else {
+            setUserData(false);
+        }
+    }, [token])
 
 
     return (
