cac579643238837fb2367ee609e74d891202b048
birgulkurtcu <157323413+birgulkurtcu@users.noreply.github.com>
2025-04-08 19:33:19 +0300

    doc controller, routes updated

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -89,5 +89,34 @@ const appointmentCancel = async (req, res) => {
         res.json({success: false, message: error.message})
     }
 }
+const doctorDashboard = async (req, res) => {
+    try{
+        const {docId} = req.body
+        const appointments = await appointmentModel.find({docId})
+        let earnings = 0
+        appointments.map((item)=>{
+            if(item.isCompleted || item.payment){
+                earnings += item.amount
+            }
+        })
+        let patients = []
 
-export {changeAvailability, doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel}
+        appointments.map((item)=>{
+            if(patients.includes(item.userId)){
+                patients.push(item.userId)
+            }
+        })
+        const dashData = {
+            earnings,
+            appointments: appointments.length,
+            patients: patients.length,
+            latestAppointments: appointments.reverse().slice(0,5)
+        }
+        res.json({success: true, dashData})
+    }catch(error){
+        console.log(error)
+        res.json({success: false, message: error.message})
+    }
+}
+
+export {changeAvailability, doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard}
--- backend/routes/doctorRoute.js
+++ backend/routes/doctorRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {appointmentsDoctor, doctorList, loginDoctor, appointmentComplete,appointmentCancel} from "../controllers/doctorController.js";
+import {appointmentsDoctor, doctorList, loginDoctor, appointmentComplete,appointmentCancel,doctorDashboard} from "../controllers/doctorController.js";
 import authDoctor from "../middlewares/authDoctor.js";
 
 const doctorRouter = express.Router()
@@ -9,4 +9,5 @@ doctorRouter.post("/login", loginDoctor)
 doctorRouter.get("/appointments", authDoctor, appointmentsDoctor)
 doctorRouter.post("/complete-appointment", authDoctor, appointmentComplete)
 doctorRouter.post("/cancel-appointment", authDoctor, appointmentCancel)
+doctorRouter.get("/dashboard", authDoctor, doctorDashboard)
 export default doctorRouter
